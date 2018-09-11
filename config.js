9de0329e2f92f40031072bdeae61e0857e50c622
Leon Fayer <lfayer@users.noreply.github.com>
2018-09-11 11:21:45 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  twitter: {
+        // account name
+        username: '',
+        // oauth credentials from Twitter
+        api_key: '',
+        api_secret: '',
+        access_token: '',
+        access_token_secret: ''
+    },
+    bot: {
+        // timeout between follower requests
+        follow_timeout: 50000 
+  }
+}
